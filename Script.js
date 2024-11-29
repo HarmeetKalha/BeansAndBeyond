fba4f65e65c2cbf56140769e451ddb9c94c25013
HarmeetKalha <harmeet.singh.kalha@gmail.com>
2024-11-30 04:21:58 +0530

    finalized the website

---
+++ Script.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    document.getElementById("popup").style.display = "flex";
+  };
+  
+  
+  function closePopup() {
+    document.getElementById("popup").style.display = "none";
+  }
+  function emailReceived(){
+    var userMail=document.emailForm.emailInput.value;
+    
+  }
+  
